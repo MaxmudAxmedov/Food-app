5b5478b301cf4690be3013cf604c8f62588e4fbf
Maxmud Axmedov <axmedovmaxmud559@gmail.com>
2022-12-26 16:15:27 +0500

    add payment changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./assets/style/style.scss";
 
 import { House } from "./components/House/House";
 import { Route, Routes } from "react-router-dom";
+import Order from "./page/Orders/Order";
+import Payment from "./page/Payment/Payment";
+import PageRoute from "./page/PageRoute/PageRoute";
 
 
 function App() {
@@ -12,11 +15,10 @@ function App() {
       <Sidebar />
       <div className="container">
         <Routes>
-          <Route path="/house/*" element={<House/>} />
+          <Route path="/house/*" element={<House />} />
         </Routes>
-      
       </div>
-      
+
     </div>
   );
 }
--- src/components/House/House.js
+++ src/components/House/House.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom"
-import Order from "../../page/Orders/Order"
+// import Order from "../../page/Orders/Order"
 import { Header } from "../Header/Header"
 import Main from "../Main/Main"
 import PageRoute from "../../page/PageRoute/PageRoute";
